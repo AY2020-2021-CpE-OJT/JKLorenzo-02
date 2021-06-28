126ef3859ac74aac51b8d58403d86eba2824fe4c
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-06-29 03:49:34 +0800

    update interfaces

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,11 +1,17 @@
 export interface StudentData {
   id: string;
   sessions: SessionData[];
+  active_log?: LogData;
 }
 
 export interface SessionData {
   id: string;
   description?: string;
-  time_in?: Date;
-  time_out?: Date;
+  logs: LogData[];
+}
+
+export interface LogData {
+  in: Date;
+  out?: Date;
+  hrs?: number;
 }
